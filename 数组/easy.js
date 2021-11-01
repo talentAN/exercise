3c803c233cb78eb55782b8c20f5fa641d2213f05
talentan <adam_an02@163.com>
2021-11-01 21:04:00 +0800

    update

--- 数组/easy.js
+++ 数组/easy.js
@@ -86,4 +86,171 @@ var plusOne = function (digits) {
   return digits;
 };
 // 118. 杨辉三角
-var generate = function (numRows) {};
+const map = {
+  1: [[1]],
+};
+var generate = function (numRows) {
+  if (map[numRows]) {
+    return map[numRows];
+  }
+  const father = generate(numRows - 1);
+  const last = father[father.length - 1];
+  const ret = [...father];
+  const next = [];
+  for (let i = 0; i < last.length; i++) {
+    next.push((last[i - 1] || 0) + last[i]);
+  }
+  next.push(last[last.length - 1]);
+  map[numRows] = [...ret, next];
+  return map[numRows];
+};
+// 119. 杨辉三角 II
+var getRow = function (rowIndex) {
+  if (map[rowIndex]) {
+    return map[rowIndex];
+  }
+  const father = getRow(rowIndex - 1);
+  const ret = [];
+  for (let i = 0; i < father.length; i++) {
+    ret.push((father[i - 1] || 0) + father[i]);
+  }
+  ret.push(father[father.length - 1]);
+  map[rowIndex] = ret;
+  return ret;
+};
+//121. 买卖股票的最佳时机
+var maxProfit = function (prices) {
+  const arr = [];
+  for (let i = 1; i < prices.length; i++) {
+    arr.push(prices[i] - prices[i - 1]);
+  }
+  let max = 0;
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (sum > 0) {
+      sum += arr[i];
+    } else {
+      sum = arr[i];
+    }
+    max = Math.max(sum, max);
+  }
+  return max;
+};
+var singleNumber = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums.lastIndexOf(nums[i]) === nums.indexOf(nums[i])) {
+      return nums[i];
+    }
+  }
+};
+var majorityElement = function (nums) {
+  const map = {};
+  const len = nums.length;
+  for (let i = 0; i < nums.length; i++) {
+    const val = nums[i];
+    map[val] = (map[val] || 0) + 1;
+    if (map[val] > len / 2) {
+      return val;
+    }
+  }
+};
+var containsDuplicate = function (nums) {
+  return Array.from(new Set(nums)).length !== nums.length;
+};
+var containsNearbyDuplicate = function (nums, k) {
+  const map = {};
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    map[num] = map[num] || [];
+    map[num].push(i);
+    if (map[num].length > 1) {
+      const arr = map[num];
+      if (arr[arr.length - 1] - arr[arr.length - 2] <= k) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+// console.info(containsNearbyDuplicate([1, 2, 3, 1], 3));
+var summaryRanges = function (nums) {
+  if (nums.length === 1) {
+    return [nums[0] + ''];
+  }
+  const ret = [];
+  for (let i = 0; i < nums.length; i++) {
+    let [cur, next] = [nums[i], nums[i + 1]];
+    let start = cur;
+    while (next === cur + 1) {
+      i++;
+      [cur, next] = [nums[i], nums[i + 1]];
+    }
+    if (start === cur) {
+      ret.push(start + '');
+    } else {
+      ret.push(`${start}->${cur}`);
+    }
+  }
+  return ret;
+};
+var missingNumber = function (nums) {
+  let ret = 0;
+  for (let i = 0; i < nums.length; i++) {
+    ret ^= nums[i];
+    ret ^= i;
+  }
+  ret ^= nums.length;
+  return ret;
+};
+var islandPerimeter = function (grid) {
+  let ret = 0;
+  grid.forEach((row, width) => {
+    row.forEach((item, height) => {
+      if (item === 1) {
+        const [left, top, right, bottom] = [
+          grid[width][height - 1],
+          grid[width - 1] && grid[width - 1][height],
+          grid[width][height + 1],
+          grid[width + 1] && grid[width + 1][height],
+        ];
+        !left && ret++;
+        !top && ret++;
+        !right && ret++;
+        !bottom && ret++;
+      }
+    });
+  });
+  return ret;
+};
+//  提莫攻击
+var findPoisonedDuration = function (timeSeries, duration) {
+  let res = 0;
+  for (let i = 1; i < timeSeries.length; i++) {
+    const gap = timeSeries[i] - timeSeries[i - 1];
+    if (gap > duration) {
+      res += duration;
+    } else {
+      res += gap;
+    }
+  }
+  return res + duration;
+};
+//500. 键盘行
+const lines = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm'];
+var findWords = function (words) {
+  const ret = [];
+  words.forEach(w => {
+    const target = lines.find(l => l.indexOf(w[0].toLowerCase()) !== -1);
+    let isTarget = true;
+    let i = 0;
+    while (isTarget && i < w.length) {
+      if (target.indexOf(w[i].toLowerCase()) === -1) {
+        isTarget = false;
+      }
+      i++;
+    }
+    isTarget && ret.push(w);
+  });
+
+  return ret;
+};
---
+++ 数组/medium.js
@@ -0,0 +1,130 @@
+function sort(a, b) {
+  return a > b ? 1 : -1;
+}
+// 18. 四数之和
+var fourSum = function (nums, target) {
+  const map = {}; // {数字: 个数}
+  nums.forEach(num => {
+    map[num] = (map[num] || 0) + 1;
+  });
+  // 排序
+  const arr = Object.keys(map)
+    .map(val => val * 1)
+    .sort(sort);
+  const ret = [];
+  for (let i = 0; i < arr.length; i++) {
+    const cdds = threeSum(arr.slice(i, arr.length), target - arr[i]);
+    if (cdds.length) {
+      cdds.forEach(cdd => {
+        const s = [arr[i], ...cdd].join(',');
+        if (ret.indexOf(s) === -1) {
+          ret.push(s);
+        }
+      });
+    }
+  }
+  // 过滤掉数字不够的数组
+  return ret
+    .map(s => s.split(',').map(num => num * 1))
+    .filter(arr => {
+      let cur = arr[0];
+      let count = 1;
+      for (let i = 1; i < arr.length; i++) {
+        if (arr[i] === cur) {
+          count++;
+        } else {
+          if (map[cur] < count) {
+            return false;
+          } else {
+            cur = arr[i];
+            count = 1;
+          }
+        }
+      }
+      return map[cur] >= count;
+    });
+};
+var threeSum = function (nums, target) {
+  const ret = [];
+  for (let i = 0; i < nums.length; i++) {
+    const cdds = twoSum(nums.slice(i, nums.length), target - nums[i]);
+    if (cdds.length) {
+      cdds.forEach(cdd => {
+        ret.push([nums[i], ...cdd]);
+      });
+    }
+  }
+  return ret;
+};
+var twoSum = function (nums, target) {
+  const ret = [];
+  let i = 0;
+  let j = nums.length - 1;
+  while (i <= j) {
+    const sum = nums[i] + nums[j];
+    if (sum === target) {
+      ret.push([nums[i], nums[j]]);
+      i++;
+      j--;
+    } else if (sum < target) {
+      i++;
+    } else {
+      j--;
+    }
+  }
+  return ret;
+};
+// console.info(fourSum([1, 0, -1, 0, -2, 2], 0));
+var nextPermutation = function (nums) {
+  const len = nums.length;
+  if (len <= 1) {
+    return nums;
+  }
+  if (len === 2) {
+    nums.reverse();
+    return nums;
+  }
+  let end = len - 1;
+  let start = len - 2;
+  let done = false;
+  while (start >= 0 && !done) {
+    let i = start;
+    while (i < end) {
+      if (nums[i] >= nums[i + 1]) {
+        i++;
+      } else {
+        break;
+      }
+    }
+    if (i === end) {
+      start--;
+    } else {
+      done = true;
+    }
+  }
+  if (start === -1) {
+    nums.reverse();
+  } else {
+    let i = nums.length - 1;
+    while (nums[i] <= nums[start]) {
+      i--;
+    }
+    [nums[start], nums[i]] = [nums[i], nums[start]];
+    let j = start + 1;
+    let k = nums.length - 1;
+    while (j < k) {
+      [nums[j], nums[k]] = [nums[k], nums[j]];
+      j++;
+      k--;
+    }
+  }
+  return nums;
+};
+// console.info(nextPermutation([2, 3, 1]));
+var searchRange = function (nums, target) {
+  if (nums[0] > target || nums[nums.length - 1] < target) {
+    return [-1, -1];
+  }
+  let start = 0;
+  let end = nums.length - 1;
+};
